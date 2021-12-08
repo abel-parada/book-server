1ee6529c7aaba2db4d3b5623a4436dbbde4afcaa
abel-parada <abel.parada.millan@gmail.com>
2021-12-08 15:06:30 +0200

    gitignore updated

--- jsonStorage/dataStorageLayer.js
+++ jsonStorage/dataStorageLayer.js
@@ -1,1 +1,22 @@
 // Third program layer. User DOES see this
+
+'use strict';
+
+const {resolveInclude} = require ('ejs');
+const {CODES,MESSAGES} = require('./statusCodes.js');
+
+const {getAllFromStorage, getOneFromStorage, addToStorage, updateStorage, removeFromStorage} = require('./storageLayer');
+
+
+// Datastorage class. We define it and export it right away
+module.exports = class Datastorage{
+    getCODES(){
+        return CODES;
+    };
+}
+
+function getCODES() {
+    return CODES;
+}
+
+console.log(getCODES());
